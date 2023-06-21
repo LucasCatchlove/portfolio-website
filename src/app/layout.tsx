90e130b6311438d19bda654731d682b9d2957c42
Lucas Catchlove <lucas@Lucass-MacBook-Pro.local>
2023-06-21 01:09:03 -0400

    added scrollable panel and added custom font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Lucas Catchlove',
-  description: 'Lucas Catchloves portfolio',
+  description: 'Lucas Catchlove\'s portfolio',
 }
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,21 @@
 import { idText } from "typescript";
+import './globals.css'
 
 export default function Home() {
   return (
-    <div className="grid lg:grid-cols-2 gap-4">
-      <header className="left-panel lg:max-h-screen lg:w-1/2 flex flex-col">
-        <div id="basics" className="py-24">
-          <h1 className="text-5xl font-bold">
-            Lucas
-            <br />
-            Catchlove
+    <div className="grid lg:grid-cols-2 gap-4 font-montserrat">
+      <header className="left-panel border-box lg:max-h-screen lg:w-1/2 flex flex-col lg:ml-24 lg:sticky lg:top-0 py-24">
+        <div id="basics">
+          <h1 className="text-5xl font-bold mb-4">Lucas Catchlove
           </h1>
-          <h2>Software Engineer</h2>
+          <h2 className="text-xl mb-12">Software Engineer</h2>
           <p>I love building software!</p>
         </div>
         <nav></nav>
         <div id="socials"></div>
       </header>
 
-      <main className="right-scroll-panel lg:w-1/2">
+      <main className="right-scroll-panel lg:w-1/2 h-[100rem] overflow-y-auto mt-24">
         <h2>hello from scroll panel!</h2>
         <section id="about"></section>
         <section id="experience"></section>
