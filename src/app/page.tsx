1d9ff49494aa4f9363fe3416dff7aaf88551f6f2
Lucas Catchlove <lucas@Lucass-MBP.hitronhub.home>
2023-08-04 18:31:52 -0400

    updated landing page margins and wrote skeleton and data model for project section and corresponding component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import './globals.css'
 
 export default function Home() {
   return (
-    <div className="grid lg:grid-cols-2 gap-4 font-montserrat">
-      <header className="left-panel border-box lg:max-h-screen flex flex-col lg:ml-24 lg:sticky lg:top-0 py-24">
+    <div className="grid lg:grid-cols-2 gap-2 font-montserrat">
+      <header className="left-panel border-box lg:max-h-screen flex flex-col lg:sticky lg:top-0 py-24 mx-12">
         <div id="basics">
           <h1 className="text-5xl font-bold mb-4">Lucas Catchlove
           </h1>
@@ -15,7 +15,7 @@ export default function Home() {
         <div id="socials"></div>
       </header>
 
-      <main className="right-scroll-panel h-[100rem] overflow-y-auto mt-24">
+      <main className="right-scroll-panel h-[100rem] overflow-y-auto mt-24 mx-12">
         <h2>hello from scroll panel!</h2>
         <section id="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</section>
         <section id="experience"></section>
---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,17 @@
+
+type ProjectProps = {
+    title: string;
+    company: string;
+    startDate: string; // might need to change this to a datetime format
+    endDate: string; // same comment as previous line
+}
+
+const ProjectCard = (data: ProjectProps) => {
+
+
+    
+}
+
+export default ProjectCard
+//fetch data server side
+
