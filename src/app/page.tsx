b635a65543da6009e1d32ebac3fc1cd7252abb39
Lucas Catchlove <lucascatchlove@gmail.com>
2023-08-27 19:06:54 -0400

    continued working on experience card and adjusted margins and padding across site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,47 @@
 import { idText } from "typescript";
-import './globals.css'
+import "./globals.css";
 import ExperienceCard from "@/components/ExperienceCard";
 
 export default function Home() {
   return (
-    <div className="grid lg:grid-cols-2 gap-2 font-montserrat">
-      <header className="left-panel border-box lg:max-h-screen flex flex-col lg:sticky lg:top-0 py-24 mx-12">
+    <div className="grid lg:grid-cols-2 font-montserrat mx-12">
+      <header className="left-panel border-box lg:max-h-screen flex flex-col lg:sticky lg:top-0 py-24 lg:ml-12">
         <div id="basics">
-          <h1 className="text-5xl font-bold mb-4">Lucas Catchlove
-          </h1>
-          <h2 className="text-xl mb-12">Software Engineer</h2>
+          <h1 className="text-5xl font-bold mb-4">Lucas Catchlove</h1>
+          <h2 className="text-xl mb-8">Software Engineer</h2>
           <p>I love building software!</p>
         </div>
         <nav></nav>
         <div id="socials"></div>
       </header>
 
-      <main className="right-scroll-panel h-[100rem] overflow-y-auto mt-24 mx-12">
-        <h2>hello from scroll panel!</h2>
-        <section id="about">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</section>
-        <section id="experience" className="mt-4"><ExperienceCard /></section>
-        <section id="projects"></section>
+      <main className="right-scroll-panel h-[100rem] overflow-y-auto lg:mt-24 lg:mr-12">
+        <section id="about">
+          <h2 className="uppercase mb-4 lg:hidden">About</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p className="mt-2">
+            Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </section>
+        <section id="experience" className="mt-48">
+          <h2 className="uppercase mb-4">Experience</h2>
+          {[1, 2, 3].map((e, i) => (
+            <ExperienceCard key={i} />
+          ))}
+          {/* <ExperienceCard /> */}
+        </section>
+        <section id="projects" className="mt-24"></section>
       </main>
     </div>
   );
--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,27 +1,39 @@
 type ExperienceProps = {
-    // title: string; 
-    // company: string; 
-    // startDate: string;
-    // endDate: string;
-    // technologies: string[];
-    // description: string;
-    // prevTitles: string[];
-}
+  // title: string;
+  // company: string;
+  // startDate: string;
+  // endDate: string;
+  // technologies: string[];
+  // description: string;
+  // prevTitles: string[];
+};
 
 const ExperienceCard = (data: ExperienceProps) => {
-    return <div className="w-full grid grid-cols-8 gap-2">
-            <div className="h-full border-solid border-2 border-sky-500 col-span-2">
-                date range here
-                
-            </div>
-            <div className="h-full flex flex-col border-solid border-2 border-sky-500 col-span-6">
-                <a href="companywebsite.com">Clickable Title</a>
-                <div>company</div>
-                <div>description text goes here</div>
-                <div>technologies are listed here</div>
-            </div>
+  return (
+    <div className="w-full grid grid-cols-8 gap-2 mt-4">
+      <div className="h-full border-none border-2 border-sky-500 col-span-2 flex flex-col">
+        <div>data range here</div>
+        <div>time here</div>
+      </div>
+      <div className="h-full flex flex-col border-none border-2 border-sky-500 col-span-6">
+        <div>
+          <a href="companywebsite.com">Clickable Title</a>
+          <a href="companywebsite.com"> Clickable Company</a>
         </div>
-
-}
+        <div className="mt-4">
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div className="mt-4">technologies are listed here</div>
+      </div>
+    </div>
+  );
+};
 
 export default ExperienceCard;
