75a895b412e7796cf0a61a3b32382f314b5bc71b
Lucas Catchlove <lucascatchlove@gmail.com>
2023-09-19 16:47:01 -0400

    added abbreviated months to experience section entries

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -11,13 +11,17 @@ export type ExperienceProps = {
   prev_titles: string[];
 };
 
+const abbMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
 const ExperienceCard = ({ ...ExperienceProps }) => {
   const start_date = new Date(ExperienceProps.start_date);
   const end_date = new Date(ExperienceProps.end_date);
   return (
     <div className="w-full grid grid-cols-8 gap-2 mt-12 hover:shadow-md p-4 rounded-lg">
       <div className="h-full col-span-2 flex flex-col opacity-50">
-        <div className="text-md">{`${start_date.getFullYear()} - ${end_date.getFullYear()}`}</div>
+        <div className="text-md">{`${abbMonths[start_date.getMonth()]} ${start_date.getFullYear()} - ${
+          abbMonths[end_date.getMonth()]
+        } ${end_date.getFullYear()}`}</div>
       </div>
       <div className="h-full flex flex-col col-span-6">
         <div>
