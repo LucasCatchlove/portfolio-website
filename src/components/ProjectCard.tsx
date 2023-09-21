5748ee90f3fb205c89f8c361c4f209223e75d560
Lucas Catchlove <lucascatchlove@gmail.com>
2023-09-21 19:28:01 -0400

    fixed html error

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -15,9 +15,6 @@ const ProjectCard = ({ technologies, title, description, imageurl, repourl, demo
     <div className="w-full grid grid-cols-8 gap-2 mt-12  hover:shadow-md rounded-lg">
       <div className="h-full border-none border-2 border-sky-500 col-span-2 flex flex-col">
         {imageurl ? <Image src={imageurl} width={200} height={100} alt="project picture" /> : "no image available"}
-    <div className="w-full grid grid-cols-8 gap-2 mt-12 hover:shadow-md rounded-lg p-4">
-      <div className="h-full border-none border-2 border-sky-500 col-span-2 flex flex-col mr-2">
-        {imageurl ? <Image className="rounded-sm" src={imageurl} width={200} height={200} alt="project picture" /> : "no image available"}
       </div>
       <div className="h-full flex flex-col border-none border-2 border-sky-500 col-span-6">
         <a className="text-teal-200 text-bold" href={repourl}>
